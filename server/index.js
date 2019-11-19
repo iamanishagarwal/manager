8c7875432ad0b104a1bdec4051d43285c2149286
iamanishagarwal <iamanishagarwal@gmail.com>
2019-11-19 22:55:40 +0530

    feat: Setup the index.js for the routes and body data in the request

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const employee = require('./routes/employee')
 
 require('./startup/db')()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use('/api/employee', employee)
 
 let port = process.env.PORT || 3000
 app.listen(port, () => console.log(`The app is listening on port : ${port}`))
