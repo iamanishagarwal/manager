cdd490b8268c73bbf85f438e032d2d9bc6d40092
iamanishagarwal <iamanishagarwal@gmail.com>
2019-11-20 07:08:28 +0530

    fix: Change the default port from 3000 to 5000

--- server/index.js
+++ server/index.js
@@ -8,5 +8,5 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/api/employee', employee)
 
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 5000
 app.listen(port, () => console.log(`The app is listening on port : ${port}`))
